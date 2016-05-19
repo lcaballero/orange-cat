f2b5d343d701d4b98aaa8fcb0c0a58a73bdfbff1
lcaballero <lucas.caballero@logrhythm.com>
2016-05-18 22:57:38 -0600

    Adding css

--- main.go
+++ main.go
@@ -6,44 +6,10 @@ import (
 	"github.com/codegangsta/cli"
 
 	"github.com/noraesae/orange-cat"
-	"io/ioutil"
-	"path/filepath"
-	"fmt"
-	"sort"
 )
 
 func main() {
-	var paths sort.StringSlice
-	css, paths, err := Css()
-	if err != nil {
-		panic(err)
-	}
-	paths.Sort()
-	fmt.Printf("Css Path: %s\n", css)
-	fmt.Printf("Path Count: %d\n", len(paths))
-	for _, p := range paths {
-		fmt.Println(p)
-	}
-}
-
-func CssPath() string {
-	home := os.Getenv("HOME")
-	dir := filepath.Join(home, ".orange")
-	return dir
-}
-
-func Css() (string, []string, error) {
-	dir := CssPath()
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return "", nil, err
-	}
-	paths := make([]string, 0)
-	for _,f := range files {
-		path := filepath.Join(dir, f.Name())
-		paths = append(paths, path)
-	}
-	return dir, paths, nil
+	pp()
 }
 
 func pp() {
